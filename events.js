4ed54ce02bba70d7f30206edddc3a5785f2c2bca
Danish Umair Ahmad <danishuahmad@gmail.com>
2022-05-17 20:53:17 +0200

    Initial render of data completed

--- events.js
+++ events.js
@@ -1,3 +1,33 @@
+function initialRender(){
+	const slots = [{fromHour: 0, toHour: 2}, {fromHour: 5, toHour: 10}, {fromHour: 12, toHour: 16}, {fromHour: 18, toHour: 23}];
+	
+	for( let i=0; i<slots.length; i++ ){
+		const slot = slots[i];
+
+		const clickedElement = document.getElementById("slot-" + slot.fromHour);
+		const borderLineElement = document.getElementById("border-line-" + slot.fromHour);
+
+		const slotOffsetData = getOffset(clickedElement)
+		const lineOffsetData = getOffset(borderLineElement)
+		const lineDimensionsData = getDimensions(borderLineElement)
+
+		const newElement = document.createElement('div');
+		newElement.setAttribute("id", "slot-box-" + slot.fromHour);
+		newElement.setAttribute("class", "resizable-slot-box");
+		newElement.setAttribute(SLOT_BOX_ATTR, slot.fromHour);
+	
+		newElement.style.width = lineDimensionsData.width + "px";
+		newElement.style.top = slotOffsetData.y + "px";
+		newElement.style.left = lineOffsetData.x + "px";
+		newElement.style.height = ((slot.toHour - slot.fromHour)*SLOT_BOX_HEGHT)+"px";
+		//  add handle (dots) in border line
+		newElement.innerHTML = `<div class="handle-container"><div id="handle-top-${slot.fromHour}" class="slot-box-handle handle-top"></div>
+	  <div id="handle-bottom-${slot.fromHour}" class="slot-box-handle handle-bottom"></div></div>`
+	
+		clickedElement.append(newElement);
+
+	}
+}
 function onSlotClick(index) {
 	index = Number(index)
 	//  check if slot already box already created for given index
